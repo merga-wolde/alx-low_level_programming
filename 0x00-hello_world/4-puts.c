77de77787f5886fb8e8a2503e18c5a9ed50dc227
merga-wolde <mergawolde@gmail.com>
2022-03-10 18:02:56 +0300

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	/*
+	 *main - prints exactly Programming is like building a multilingual puzzle
+	 *
+	 * return: 0
+	 */
+puts("programming is like building a multilingual puzzle");
+	return (0);
+}
