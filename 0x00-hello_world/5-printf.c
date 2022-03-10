0e6bd0a8d7387cc833005e0dd89a3275dfe41763
merga-wolde <mergawolde@gmail.com>
2022-03-10 18:32:22 +0300

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- printing text with printf
+ *
+ * Return: returns message
+ * Description: description of the code
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
