cf9b473f13ff22055fc1bdcb04c5d5da60072255
merga-wolde <mergawolde@gmail.com>
2022-03-11 19:05:24 +0300

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- generate random number
+ *
+ * Return: return the value
+ * Description: description of the code
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar (i);
+		putchar (', ');
+	}
+	putchar ('\n');
+	return (0);
+}
