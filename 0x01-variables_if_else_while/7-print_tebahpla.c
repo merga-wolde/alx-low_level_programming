44da3cb919f14776373f98bdb4b35d0900c2ac82
merga-wolde <mergawolde@gmail.com>
2022-03-11 18:48:07 +0300

    print reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- generate random number
+ *
+ * Return: return the value
+ * Description: description of the code
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a')
+	{
+		putchar (alpha);
+	}
+	putchar ('\n');
+	return (0);
+}
