7aabfd0ed6d457b405fc73ba70e9173bf3fac5c6
merga-wolde <mergawolde@gmail.com>
2022-03-11 18:42:45 +0300

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- generate random number
+ *
+ * Return: return the value
+ * Description: description of the code
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+	return (0);
+}
