3d063e2f498fbc8ddcb368f3b1dab894e4f808d7
merga-wolde <mergawolde@gmail.com>
2022-03-11 18:14:53 +0300

    alphabets both

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- generate random number
+ *
+ * Return: return the value
+ * Description: description of the code
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar (alphabet);
+	}
+	for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar (alpha);
+	}
+	putchar ('\n');
+	return (0);
+}
