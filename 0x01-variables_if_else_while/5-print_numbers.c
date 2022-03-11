e051b82b836f5e3a7a179993e0d0a1a1a00b1a16
merga-wolde <mergawolde@gmail.com>
2022-03-11 18:35:10 +0300

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- generate random number
+ *
+ * Return: return the value
+ * Description: description of the code
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf(i);
+	}
+	printf('\n');
+	return (0);
+}
