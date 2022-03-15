17a3c50ab139ffdb24040c5188c9bd3bca6d9797
merga-wolde <mergawolde@gmail.com>
2022-03-15 13:37:11 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check lower case
+ * @c : character to check the cases
+ *
+ * Return: returns 0 or 1
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
