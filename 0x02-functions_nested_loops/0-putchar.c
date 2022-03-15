20884ae36785f8120b480061f55aa221872a1454
merga-wolde <mergawolde@gmail.com>
2022-03-15 09:23:05 +0300

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- program for printing output
+ *
+ * Return: returns a 0
+ * Description: printing __putchar
+ */
+int main(void)
+{
+	putchar('_putchar');
+	return (0);
+}
