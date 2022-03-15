1f1a043cc4b97f5d02dff531a45f94dde1900618
merga-wolde <mergawolde@gmail.com>
2022-03-15 14:32:34 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - check the numbers abs and returns
+ * @n : the num to check
+ *
+ * Return: returns 0 or 1
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}
