0959f8953559080558f35bf1f789d2c75e1e6bfb
merga-wolde <mergawolde@gmail.com>
2022-03-24 18:23:30 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,8 +11,8 @@ int _strcmp(char *s1, char *s2)
 int i;
 
 /**
- * We have reached the end of S1 OR we have reached the end of S2
- * 
+ * _strcmp - We have reached the end of S1 OR we have reached the end of S2
+ * @s2: string 2
 */
 
 for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
